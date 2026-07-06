b947f031d3cc200552ff26e2cd8ba2c391db9651
za-stanleyxu <xuxiang@zhongan.io>
2026-07-06 18:11:23 +0800

    edit config

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 {
-  "PROJECTS_DIR": "/Users/za-stanlexu/Desktop/ff/h/projects",
+  "PROJECTS_DIR": "/home/ubuntu/claw-workspace/projects",
+  "SRE_DIR":"/home/ubuntu/claw-workspace/sre",
   "WEBDESIGN_DIR": ".webdesign",
   "TASKS_DIR": "tasks",
   "WORKFLOW_VERSION": "v1",
